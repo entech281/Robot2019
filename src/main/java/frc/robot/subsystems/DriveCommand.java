3391c4e2391906dea7c7818886a8aa2d35cd5a4b
Dave Cowden <dave.cowden@gmail.com>
2019-01-31 17:15:10 -0500

    template for alignment code

---
+++ src/main/java/frc/robot/subsystems/DriveCommand.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+/**
+ *
+ * A command for a drive system
+ */
+public class DriveCommand {
+    
+    public DriveCommand(double x, double y, double z){
+        
+    }
+    
+    public double getX(){
+        return 0.0;
+    }
+    
+    public double getY(){
+        return 0.0;
+    }
+    
+    public double getZ(){
+        return 0.0;
+    }
+}
