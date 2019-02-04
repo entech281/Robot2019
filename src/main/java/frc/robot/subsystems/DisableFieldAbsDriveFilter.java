0a78767e39044cef727b4025fc0a18ccb618ce33
Michael Andrews <mandrews@mandrews.org>
2019-02-03 22:45:33 -0500

    Add additional files -- commit -a doesn't add new files by default

---
+++ src/main/java/frc/robot/subsystems/DisableFieldAbsDriveFilter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frc.robot.subsystems;
+
+import java.lang.Math;
+
+/**
+ * SImply drops the z component
+ * @author dcowden
+ */
+public class DisableFieldAbsDriveFilter extends DriveFilter {
+
+    @Override
+    public DriveCommand doFilter(DriveCommand input) {
+        return new DriveCommand(input.getX(), input.getY(), input.getZ(), 0.0);          
+    }
+    
+}
---
+++ src/main/java/frc/robot/subsystems/NudgeLeftDriveFilter.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frc.robot.subsystems;
+
+import java.lang.Math;
+
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * SImply drops the z component
+ * @author dcowden
+ */
+public class NudgeLeftDriveFilter extends DriveFilter {
+    static private double nudgeTime = 0.1;
+    static private double nudgePower = 0.5;
+    private Timer m_timer = new Timer();
+
+    @Override
+    public void enable() {
+        if (isEnabled()) {
+            return;
+        }
+        m_timer.stop();
+        m_timer.reset();
+        m_timer.start();
+        super.enable();
+    }
+    
+    @Override
+    public DriveCommand doFilter(DriveCommand input) {
+        if (m_timer.get() > nudgeTime) {
+            disable();
+            return input;
+        }
+        return new DriveCommand(0.0, nudgePower, 0.0, 0.0);
+    }
+    
+}
