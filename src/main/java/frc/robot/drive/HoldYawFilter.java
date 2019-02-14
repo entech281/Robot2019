5f55a3c96c7a450e14fe801cb719ea4f830ee7b8
Michael Andrews <mandrews@mandrews.org>
2019-02-13 19:09:14 -0500

    Move Yaw PIDSource to the correct place (the NavX subsystem)

--- src/main/java/frc/robot/drive/HoldYawFilter.java
+++ src/main/java/frc/robot/drive/HoldYawFilter.java
@@ -10,16 +10,13 @@ package frc.robot.drive;
 import frc.robot.Robot;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
-import edu.wpi.first.wpilibj.PIDSource;
-import edu.wpi.first.wpilibj.PIDSourceType;
 
 /**
  * Add your docs here.
  */
-public class HoldYawFilter extends DriveFilter implements PIDSource,PIDOutput {
+public class HoldYawFilter extends DriveFilter implements PIDOutput {
   Robot robot;
   PIDController yaw_pid;
-  PIDSourceType pidSourceType = PIDSourceType.kRate;
   double pid_twist;
   double Kp = 0.03;
   double Ki = 0.0001;
@@ -28,7 +25,7 @@ public class HoldYawFilter extends DriveFilter implements PIDSource,PIDOutput {
   public HoldYawFilter(Robot robot) {
     super(false);
     this.robot = robot;
-    yaw_pid = new PIDController(Kp, Ki, Kd, this, this);
+    yaw_pid = new PIDController(Kp, Ki, Kd, this.robot.getNavXSubsystem(), this);
     yaw_pid.setInputRange(-180.0, 180.0);
     yaw_pid.setContinuous(true);
     yaw_pid.setOutputRange(-1.0, 1.0);
@@ -48,21 +45,6 @@ public class HoldYawFilter extends DriveFilter implements PIDSource,PIDOutput {
   @Override
   protected void onDisable() {
     yaw_pid.disable();
-  }
-
-  @Override
-  public PIDSourceType getPIDSourceType() {
-    return pidSourceType;
-  }
-
-  @Override
-  public void setPIDSourceType(PIDSourceType pidSource) {
-    pidSourceType = pidSource;
-  }
-
-  @Override
-  public double pidGet() {
-    return this.robot.getNavXSubsystem().getAngle();
   }
 
   @Override
