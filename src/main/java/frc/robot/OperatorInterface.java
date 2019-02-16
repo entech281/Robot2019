da7211b7caeff204e86a745b5600d4e313bbcd56
Santiago Gutierrez <santiago.gutierrez@staff.gtchs.org>
2019-02-16 12:22:43 -0500

    test

--- src/main/java/frc/robot/OperatorInterface.java
+++ src/main/java/frc/robot/OperatorInterface.java
@@ -1,8 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
