f1b73345832908b1a43f9807600674ce313e0f73
Dave Cowden <dave.cowden@gmail.com>
2019-02-24 10:40:36 -0500

    this centralizes code that modifies drive input so we can see it all together

---
+++ src/main/java/frc/robot/drive/DriveInputAggregator.java
@@ -0,0 +1,30 @@
+package frc.robot.drive;
+
+
+/**
+ *
+ * @author dcowden
+ */
+public class DriveInputAggregator {
+    
+    public DriveInput mergeTelemetry(DriveInput input, DriveInput navx, DriveInput vision, DriveInput sensors){
+        DriveInput result = input.copy();
+       
+        if (navx.isValid()) {
+          result.setFieldAngle(navx.getFieldAngle());
+        }
+        
+        if (vision.isValid()) {
+          result.setTargetX(vision.getTargetX());
+          result.setTargetY(vision.getTargetY());
+        }
+
+        if (sensors.isValid()) {
+          result.setFieldAngle(sensors.getFieldAngle());
+          result.setTargetX(sensors.getTargetX());
+          result.setTargetY(sensors.getTargetY());
+        }
+
+        return input;       
+    }
+}
